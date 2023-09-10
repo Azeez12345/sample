99e5e7ce2af5674f1e79ba7ceab26b1c72c337ea
Azeez12345 <azizrocky1951@gmail.com>
2023-09-10 12:10:57 +0530

    Lcommit

--- 17Q/ticketfile.c
+++ 17Q/ticketfile.c
@@ -1,34 +1,68 @@
-#include<stdio.h>
-#include<sys/types.h>
-#include<unistd.h>
-#include<fcntl.h>
-#include<string.h>
 
-int main() {
-  struct flock lock;
-  int fd=open("result.txt", O_RDWR);
-  lock.l_whence = SEEK_SET;
-  lock.l_start = 0;
-  lock.l_len = 0;
-  int buffer;
-  lock.l_type=F_RDLCK;
-  printf("Read lock set\n");
-  fcntl(fd, F_SETLKW, &lock);
-  read(fd, &buffer, sizeof(int));
-  lseek(fd, -4, SEEK_CUR);
-  printf("Read ticket no: %d\n", buffer);
-  buffer++;
-  lock.l_type=F_WRLCK;
-  fcntl(fd, F_SETLKW, &lock);
-  printf("Write lock set\n");
-  write(fd, &buffer, sizeof(int));
-  lseek(fd, -4, SEEK_CUR);
-  read(fd, &buffer, sizeof(int));
-  printf("Read after writing: %d\n", buffer);
-  printf("Press return to unlock...");
-  getchar();
-  lock.l_type=F_UNLCK;
-  fcntl(fd, F_SETLKW, &lock);
-  close(fd);
-  return 0;
+#include <sys/types.h> 
+#include <sys/stat.h>  
+#include <fcntl.h>     
+#include <unistd.h>    
+#include <stdio.h>     
+
+void main()
+{
+    int fileDescriptor, fcntlStatus;
+    ssize_t readBytes, writeBytes;
+    off_t lseekOffset;
+    int buffer;
+    struct flock ticketLock;
+
+    fileDescriptor = open("result.txt", O_CREAT | O_RDWR, S_IRWXU);
+
+    if (fileDescriptor == -1)
+        perror("Error while opening the file!");
+    else
+    {
+        ticketLock.l_type = F_WRLCK;
+        ticketLock.l_pid = getpid();
+        ticketLock.l_whence = SEEK_SET;
+        ticketLock.l_len = 0;
+        ticketLock.l_start = 0;
+
+        fcntlStatus = fcntl(fileDescriptor, F_GETLK, &ticketLock);
+
+        if (fcntlStatus == -1)
+            perror("Error while getting lock status!");
+        else
+        {
+                    readBytes = read(fileDescriptor, &buffer, sizeof(int));
+                    if (readBytes == -1)
+                        perror("Error while reading ticket!");
+                    else if (readBytes == 0)
+                    {
+                        buffer = 1;
+                        writeBytes = write(fileDescriptor, &buffer, sizeof(int));
+                        if (writeBytes == -1)
+                            perror("Error while writing to ticket!");
+                        else
+                            printf("Your ticket number is %d\n", buffer);
+                    }
+                    else
+                    {
+                        lseekOffset = lseek(fileDescriptor, 0, SEEK_SET);
+                        if (lseekOffset == -1)
+                            perror("Error while seeking!");
+                        else
+                        {
+                            buffer += 1;
+                            writeBytes = write(fileDescriptor, &buffer, sizeof(int));
+                            printf("Your ticket number is: %d\n", buffer);
+                        }
+                    }
+                    ticketLock.l_type = F_UNLCK;
+                    fcntlStatus = fcntl(fileDescriptor, F_SETLKW, &ticketLock);
+                    if (fcntlStatus == -1)
+                        perror("Error releasing lock!");
+                    else
+                        printf("Done!\n");
+                
+        }
+        close(fileDescriptor);
+    }
 }
